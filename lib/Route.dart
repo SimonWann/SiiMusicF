ca093f7729123ebaa31eb92e0844451b01cae79b
王思曼 <simooowann@gmail.com>
2022-11-22 23:19:45 +0800

    init

---
+++ lib/Route.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SiiRoute extends StatefulWidget{
+  const SiiRoute({Key? key});
+
+  @override
+  _SiiRouteState createState() => _SiiRouteState();
+}
+
+class _SiiRouteState extends State<SiiRoute> {
+  @override
+  void initState() {
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Row(
+      children: [
+        Text('Hello')
+      ],
+    );
+  }
+}
